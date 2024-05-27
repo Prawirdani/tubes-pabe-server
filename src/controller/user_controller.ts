d74052bda1a4fdc6c115c330824ac9f0380578a5
Prawirdani <prawirdani@gmail.com>
2024-05-28 05:36:05 +0700

    guard user list endpoint

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -1,21 +1,21 @@
 import { Router, NextFunction, Request, Response } from 'express';
 import { MakeResponse } from '../utils/response';
 import db from '../db/conn';
-
-const getUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const users = await db.query.users.findMany({
-            columns: {
-                password: false,
-            },
-        });
-        res.status(200).json(MakeResponse(users));
-    } catch (error) {
-        next(error);
-    }
-};
+import { AuthAccessToken } from './middleware/authenticate';
 
 const userRoute = Router();
-userRoute.get('/users', getUser);
-
+userRoute.get('/users', AuthAccessToken, getUsers);
 export default userRoute;
+
+async function getUsers(req: Request, res: Response, next: NextFunction) {
+  try {
+    const users = await db.query.users.findMany({
+      columns: {
+        password: false,
+      },
+    });
+    res.status(200).json(MakeResponse(users));
+  } catch (error) {
+    next(error);
+  }
+}

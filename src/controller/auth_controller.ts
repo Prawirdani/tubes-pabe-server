f6ea6b95ce687ca6e0b2a616eabccb12a5017ba9
Prawirdani <prawirdani@gmail.com>
2024-05-28 05:29:00 +0700

    update: seperate auth token middlewares

--- src/controller/auth_controller.ts
+++ src/controller/auth_controller.ts
@@ -3,56 +3,55 @@ import authService from '../service/auth_service';
 import { validateRequest } from '../utils/validator';
 import { authLoginSchema, authRegisterSchema } from '../schemas/auth_schema';
 import { MakeResponse } from '../utils/response';
-import { Authenticate } from './middleware/authenticate';
-
-const register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        validateRequest(authRegisterSchema, req);
-        const { nama, email, password } = req.body;
-
-        const newUser = await authService.register({ nama, email, password });
-
-        res.status(201).json(MakeResponse(newUser, 'User created'));
-    } catch (error) {
-        next(error);
-    }
-};
-
-const login = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        validateRequest(authLoginSchema, req);
-        const { email, password } = req.body;
-
-        const { ...tokens } = await authService.login({ email, password });
-
-        res.status(200).json(MakeResponse(tokens, 'Berhasil login!'));
-    } catch (error) {
-        next(error);
-    }
-};
-
-const currentUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.status(200).json(MakeResponse(req.user, 'Berhasil mendapatkan data user'));
-    } catch (error) {
-        next(error);
-    }
-};
-
-const refresh = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const userId = req.user.id;
-        const { ...tokens } = await authService.refreshToken(userId!);
-        res.status(200).json(MakeResponse(tokens, 'Berhasil refresh token'));
-    } catch (error) {
-        next(error);
-    }
-};
+import { AuthAccessToken, AuthRefreshToken } from './middleware/authenticate';
 
 const authRoute = Router();
-
 authRoute.post('/auth/register', register);
 authRoute.post('/auth/login', login);
-authRoute.get('/auth/current', Authenticate(), currentUser);
-authRoute.get('/auth/refresh', Authenticate('refresh'), refresh);
+authRoute.get('/auth/current', AuthAccessToken, currentUser);
+authRoute.get('/auth/refresh', AuthRefreshToken, refresh);
 export default authRoute;
+
+async function register(req: Request, res: Response, next: NextFunction) {
+  try {
+    validateRequest(authRegisterSchema, req);
+    const { nama, email, password } = req.body;
+
+    const newUser = await authService.register({ nama, email, password });
+
+    res.status(201).json(MakeResponse(newUser, 'User created'));
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    validateRequest(authLoginSchema, req);
+    const { email, password } = req.body;
+
+    const { ...tokens } = await authService.login({ email, password });
+
+    res.status(200).json(MakeResponse(tokens, 'Berhasil login!'));
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function currentUser(req: Request, res: Response, next: NextFunction) {
+  try {
+    res.status(200).json(MakeResponse(req.user, 'Berhasil mendapatkan data user'));
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function refresh(req: Request, res: Response, next: NextFunction) {
+  try {
+    const userId = req.user.id;
+    const { ...tokens } = await authService.refreshToken(userId!);
+    res.status(200).json(MakeResponse(tokens, 'Berhasil refresh token'));
+  } catch (error) {
+    next(error);
+  }
+}
--- src/controller/middleware/authenticate.ts
+++ src/controller/middleware/authenticate.ts
@@ -2,26 +2,29 @@ import { NextFunction, Request, Response } from 'express';
 import { ErrUnauthorized } from '../../utils/error';
 import { verifyToken } from '../../utils/jwt';
 
-export function Authenticate(type: 'access' | 'refresh' = 'access') {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const authHeader = req.headers.authorization;
-            if (!authHeader) {
-                throw ErrUnauthorized('Missing auth token');
-            }
+function Authenticate(type: 'access' | 'refresh' = 'access') {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        throw ErrUnauthorized('Missing auth token');
+      }
 
-            const token = authHeader.split(' ')[1];
-            const decoded = verifyToken(token);
+      const token = authHeader.split(' ')[1];
+      const decoded = verifyToken(token);
 
-            // Check if the token type is the same as the expected type
-            if (type !== decoded.type) {
-                throw ErrUnauthorized(`Invalid token type, expected ${type} token`);
-            }
+      // Check if the token type is the same as the expected type
+      if (type !== decoded.type) {
+        throw ErrUnauthorized(`Invalid token type, expected ${type} token`);
+      }
 
-            req.user = decoded;
-            next();
-        } catch (error) {
-            next(error);
-        }
-    };
+      req.user = decoded;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 }
+
+export const AuthAccessToken = Authenticate('access');
+export const AuthRefreshToken = Authenticate('refresh');

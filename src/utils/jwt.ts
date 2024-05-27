13f09f18dfecdbbd4fabb7334ce7b3c454b4d8f4
Prawirdani <prawirdani@gmail.com>
2024-05-28 02:02:16 +0700

    add jwt utility

---
+++ src/utils/jwt.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import jsonwebtoken, { JwtPayload } from 'jsonwebtoken';
+import { ErrUnauthorized } from './error';
+
+const secret = process.env.JWT_SECRET ?? 'secret';
+export function generateToken(payload: any, type: 'access' | 'refresh' = 'access') {
+    const expiresIn =
+        type === 'access'
+            ? process.env.ACCESS_TOKEN_EXPIRES ?? '1h'
+            : process.env.REFRESH_TOKEN_EXPIRES ?? '7d';
+
+    // Add token type to payload
+    payload['type'] = type;
+
+    return jsonwebtoken.sign(payload, secret, { expiresIn });
+}
+
+export function verifyToken(token: string) {
+    try {
+        return jsonwebtoken.verify(token, secret) as JwtPayload;
+    } catch (error) {
+        throw ErrUnauthorized('Invalid or expired token');
+    }
+}

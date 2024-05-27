e632e39f9c088f5d22bdaffda8f3ac752c0a7725
Prawirdani <prawirdani@gmail.com>
2024-05-27 21:30:51 +0700

    add console.error on error middleware

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,21 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import { ApiError } from '../utils/error';
 export function ErrorMiddleware(
-	err: Error,
-	req: Request,
-	res: Response,
-	next: NextFunction,
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
 ) {
-	if (err instanceof ApiError) {
-		res.status(err.status).json({
-			error: {
-				status: err.status,
-				message: err.message,
-			},
-		});
-	} else {
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
+    console.error(err);
+    if (err instanceof ApiError) {
+        res.status(err.status).json({
+            error: {
+                status: err.status,
+                message: err.message,
+            },
+        });
+    } else {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 export const ErrNotFound = (msg: string) => new ApiError(msg, 404);
+export const ErrBadRequest = (msg: string) => new ApiError(msg, 400);

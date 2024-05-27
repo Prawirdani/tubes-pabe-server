f3f6fecabc0352edee26e9a2611efa0e605381b9
Prawirdani <prawirdani@gmail.com>
2024-05-27 16:19:08 +0700

    add express json middleware

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,8 @@ import { ErrorMiddleware } from './middleware/error';
 import { MakeResponse } from './utils/response';
 import { MapUserRoutes } from './controller/user_controller';
 
-const index = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.status(200).json(MakeResponse(null, 'Hello, World!'));
-    } catch (error) {
-        next(error);
-    }
-};
-
 const app = express();
+app.use(express.json());
 
 MapUserRoutes(app);
 
@@ -24,3 +17,11 @@ const APP_PORT = process.env.APP_PORT ?? 3000;
 app.listen(APP_PORT, () => {
     console.log(`Server is running on port ${APP_PORT}`);
 });
+
+function index(req: Request, res: Response, next: NextFunction) {
+    try {
+        res.status(200).json(MakeResponse(null, 'Hello, World!'));
+    } catch (error) {
+        next(error);
+    }
+}

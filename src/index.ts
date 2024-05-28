5aa3fe16c17cf7ee6caebe1158664f4f59c19dce
Prawirdani <prawirdani@gmail.com>
2024-05-28 10:09:44 +0700

    add gzip compression

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
+import { ErrorHandler } from './controller/middleware/error';
 import { MakeResponse } from './utils/response';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import apiRoute from './controller/routes';
-import { ErrorHandler } from './controller/middleware/error';
+import compression from 'compression';
 
 const app = express();
 
+app.use(compression());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.status(200).json(MakeResponse(null, 'Hello, World!'));
-    } catch (error) {
-        next(error);
-    }
+  try {
+    res.status(200).json(MakeResponse(null, 'Hello, World!'));
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use(apiRoute);
@@ -26,5 +28,5 @@ app.use(ErrorHandler);
 
 const APP_PORT = process.env.APP_PORT ?? 3000;
 app.listen(APP_PORT, () => {
-    console.log(`Server is running on port ${APP_PORT}`);
+  console.log(`Server is running on port ${APP_PORT}`);
 });

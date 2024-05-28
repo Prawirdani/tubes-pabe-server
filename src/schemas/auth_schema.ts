3a2158f0d083ca86585b3c76babb5bed86bdb81b
Prawirdani <prawirdani@gmail.com>
2024-05-29 06:41:59 +0700

    update: remove min password chars in login schema

--- src/schemas/auth_schema.ts
+++ src/schemas/auth_schema.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 export const authRegisterSchema = z.object({
-    nama: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
+  nama: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
 });
 export type AuthRegisterSchema = z.infer<typeof authRegisterSchema>;
 
 export const authLoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+  email: z.string().email(),
+  password: z.string(),
 });
 
 export type AuthLoginSchema = z.infer<typeof authLoginSchema>;
